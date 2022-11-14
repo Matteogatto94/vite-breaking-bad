d5ae7b7e738b4cbeb4c69cb64e1e05488d87dff5
Matteogatto94 <cosenza94@live.it>
2022-11-14 21:51:49 +0100

    finish exercise

--- src/components/CharactersList.vue
+++ src/components/CharactersList.vue
@@ -1,12 +1,16 @@
 <script>
 import { store } from '../store.js';
+import SingleCharacter from './SingleCharacter.vue'
 export default {
     name: 'CharactersList',
+    components: {
+        SingleCharacter
+    },
     data() {
         return {
-            store
+            store,
         }
-    }
+    },
 }
 </script>
 
@@ -15,15 +19,7 @@ export default {
         <div class="container">
             <div class="cards">
                 <div class="row row-cols-md-5 g-4">
-                    <div class="col" v-for="character in store.characters">
-                        <div class="card">
-                            <img :src="character.img" :alt="character.name">
-                            <h2>{{ character.name }}</h2>
-                            <p class="category">{{ character.category }}</p>
-                            <p class="status">{{ character.status }}</p>
-                        </div>
-
-                    </div>
+                    <SingleCharacter :character="character" v-for="character in store.characters" />
                 </div>
             </div>
 
@@ -33,36 +29,5 @@ export default {
 </template>
 
 <style lang="scss">
-.Characters {
-    padding: 1rem;
 
-    .cards {
-        margin-top: 1rem;
-        background-color: white;
-        padding: 1.5rem;
-    }
-
-    .card {
-        align-items: center;
-        padding: 1rem;
-        height: 450px;
-        width: 250px;
-        background-color: #2E3A46;
-
-        h2 {
-            color: white;
-            font-size: 1rem;
-            padding: 1rem;
-        }
-
-        p {
-            color: darkgrey;
-        }
-    }
-
-    img {
-        width: 200px;
-        height: 250px;
-    }
-}
 </style>
---
+++ src/components/SingleCharacter.vue
@@ -0,0 +1,63 @@
+<script>
+import { store } from '../store.js';
+export default {
+    name: 'SingleCharacter',
+    props: {
+        character: Object
+    },
+
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div class="col">
+        <div class="card">
+            <img :src="character.img" :alt="character.name">
+            <h2>{{ character.name }}</h2>
+            <p class="category">{{ character.category }}</p>
+            <p class="status">{{ character.status }}</p>
+        </div>
+
+    </div>
+</template>
+
+<style lang="scss">
+.Characters {
+    padding: 1rem;
+
+    .cards {
+        margin-top: 1rem;
+        background-color: white;
+        padding: 1.5rem;
+    }
+
+    .card {
+        align-items: center;
+        padding: 1rem;
+        height: 450px;
+        width: 250px;
+        background-color: #2E3A46;
+
+        h2 {
+            color: white;
+            font-size: 1rem;
+            padding: 1rem;
+        }
+
+        p {
+            color: darkgrey;
+        }
+    }
+
+    img {
+        width: 200px;
+        height: 250px;
+    }
+}
+</style>
