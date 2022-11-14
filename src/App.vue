f533f555f82664ccc62cbad5a84b8e916400fe88
Matteogatto94 <cosenza94@live.it>
2022-11-14 15:22:47 +0100

    create a component AppHeader with title

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
 
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template>
-
-
+  <AppHeader />
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+
+    <header>
+        <h1>Breaking Bad Api</h1>
+    </header>
+
+</template>
+
+<style lang="scss">
+header {
+    h1 {
+        color: white;
+    }
+}
+</style>
