2757eb47a287f3eb5e710acf01a30bcc59bf6650
Matteogatto94 <cosenza94@live.it>
2022-11-14 15:37:41 +0100

    add SelectForm components

---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+import SelectForm from './SelectForm.vue'
+export default {
+    name: 'AppMain',
+    components: {
+        SelectForm
+    }
+}
+</script>
+
+<template>
+
+    <main>
+
+        <SelectForm />
+
+    </main>
+
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/SelectForm.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'SelectForm'
+}
+</script>
+
+<template>
+
+    <div class="container">
+        <select>
+            <option selected>Select Category</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>
+    </div>
+
+</template>
+
+<style lang="scss">
+select {
+    padding: 0.5rem 0.5rem;
+    border-radius: 10px;
+}
+</style>
